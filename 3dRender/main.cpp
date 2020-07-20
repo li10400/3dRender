7c969f3e51219ab5907887fc23da16c42f09c389
1 <2>
2020-07-20 23:02:04 +0800

    Initial Commit

---
+++ 3dRender/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3dRender
+//
+//  Created by 李金金 on 2020/7/20.
+//  Copyright © 2020 李金金. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
